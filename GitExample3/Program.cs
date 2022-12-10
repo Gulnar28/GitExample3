5c533dec3a7b1ebcb39be9c71bdfceca47f1c4f8
gulnarvma@outlook.com <gulnarvma@outlook.com>
2022-12-10 06:45:38 +0400

    first commit

---
+++ GitExample3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
